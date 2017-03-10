900c04def44880df401aa7ccc185362a49e2916e
Alex Strachan <alexstrachan@AlexStrachan-Mac.local>
2017-03-10 10:59:03 -0800

    early implementation

--- lang/jml.js
+++ lang/jml.js
@@ -1,24 +1,40 @@
+const uuid = require('uuid').v4;
+
 const utils = {};
 utils.decorateJmlIds = require('../utils/decorate-jml-ids');
 utils.translateToHtml = require('../utils/translate-to-html');
 
 
 class Jml {
-  constructor(document) {
+  constructor(document, query, salt = uuid()) {
     this.document = Object.assign({}, document);
+    this.query = query;
+    this.salt = salt;
     this.decorate();
+    this.render();
   }
 
   update(document) {
-    alert('were not quite there yet');
+    this.document = Object.assign({}, document);
+    // probably do some diffing instead of this...
+    this.decorate();
+    this.render();
   }
 
   decorate() {
-    utils.decorateJmlIds(this);
+    utils.decorateJmlIds(this, this.salt);
+  }
+
+  get rendered() {
+    return utils.translateToHtml(this);
   }
 
   render() {
-    return utils.translateToHtml(this);
+    const rendered = this.rendered;
+    console.log(rendered);
+    document.querySelector(this.query).innerHTML = rendered.html;
+
+    // handle the listeners
   }
 }
 
